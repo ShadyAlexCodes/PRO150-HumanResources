eec834caf5665664a3bc4fce4b51be950f0965cb
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2022-11-09 19:39:42 -0700

    Added the core project directories

--- src/main/java/com/example/pro355humanresources/Pro355HumanResourcesApplication.java
+++ src/main/java/com/example/pro355humanresources/Pro355HumanResourcesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Pro355HumanResourcesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Pro355HumanResourcesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Pro355HumanResourcesApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/pro355humanresources/Pro355HumanResourcesApplicationTests.java
+++ src/test/java/com/example/pro355humanresources/Pro355HumanResourcesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Pro355HumanResourcesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
