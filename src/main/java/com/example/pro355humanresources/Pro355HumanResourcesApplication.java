c91312e653dbc1668eafef4526b31ef02b6f68c5
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2022-11-09 19:38:06 -0700

    Initial commit

---
+++ src/main/java/com/example/pro355humanresources/Pro355HumanResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.example.pro355humanresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro355HumanResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pro355HumanResourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pro355humanresources/Pro355HumanResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pro355humanresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro355HumanResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
