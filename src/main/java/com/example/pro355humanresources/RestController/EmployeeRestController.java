bbbb99d183d7e3f25306784db7f6d164548174dc
KarasuZero <60535595+KarasuZero@users.noreply.github.com>
2022-11-10 18:17:06 -0700

    Update EmployeeRestController.java
    Test commit with hello world for EmployeeRestController

--- src/main/java/com/example/pro355humanresources/RestController/EmployeeRestController.java
+++ src/main/java/com/example/pro355humanresources/RestController/EmployeeRestController.java
@@ -1,4 +1,14 @@
 package com.example.pro355humanresources.RestController;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ERC")
 public class EmployeeRestController {
+        @RequestMapping(value = "", method = RequestMethod.GET)
+        public String OnlineConfirmation() {
+            return "Employee Rest Controller Online";
+        }
 }
