7d47232ecb517033c582813c4d9b2e91080737e1
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2022-11-10 18:19:25 -0700

    Initial API

--- src/main/java/com/example/pro355humanresources/Models/Employee.java
+++ src/main/java/com/example/pro355humanresources/Models/Employee.java
@@ -1,13 +1,120 @@
 package com.example.pro355humanresources.Models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
 public abstract class Employee {
 
-    private int employeeId;
-    private int employeeSSN;
+    @Id int employeeId;
     private String employeeFirstName;
     private String employeeLastName;
     private String employeeEmail;
-    private Position position;
+    private int employeeSSN;
+    private double employeeSalary;
+    private boolean employeeStatus;
+    private String createdDate;
+    private String modifiedDate;
 
+    public Employee() {
+    }
 
+    public Employee(int employeeId, String employeeFirstName, String employeeLastName, String employeeEmail, int employeeSSN, double employeeSalary, boolean employeeStatus, String createdDate) {
+        this.employeeId = employeeId;
+        this.employeeFirstName = employeeFirstName;
+        this.employeeLastName = employeeLastName;
+        this.employeeEmail = employeeEmail;
+        this.employeeSSN = employeeSSN;
+        this.employeeSalary = employeeSalary;
+        this.employeeStatus = employeeStatus;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeFirstName() {
+        return employeeFirstName;
+    }
+
+    public void setEmployeeFirstName(String employeeFirstName) {
+        this.employeeFirstName = employeeFirstName;
+    }
+
+    public String getEmployeeLastName() {
+        return employeeLastName;
+    }
+
+    public void setEmployeeLastName(String employeeLastName) {
+        this.employeeLastName = employeeLastName;
+    }
+
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
+
+    public void setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
+
+    public int getEmployeeSSN() {
+        return employeeSSN;
+    }
+
+    public void setEmployeeSSN(int employeeSSN) {
+        this.employeeSSN = employeeSSN;
+    }
+
+    public double getEmployeeSalary() {
+        return employeeSalary;
+    }
+
+    public void setEmployeeSalary(double employeeSalary) {
+        this.employeeSalary = employeeSalary;
+    }
+
+    public boolean isEmployeeStatus() {
+        return employeeStatus;
+    }
+
+    public void setEmployeeStatus(boolean employeeStatus) {
+        this.employeeStatus = employeeStatus;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", employeeFirstName='" + employeeFirstName + '\'' +
+                ", employeeLastName='" + employeeLastName + '\'' +
+                ", employeeEmail='" + employeeEmail + '\'' +
+                ", employeeSSN=" + employeeSSN +
+                ", employeeSalary=" + employeeSalary +
+                ", employeeStatus=" + employeeStatus +
+                ", createdDate='" + createdDate + '\'' +
+                ", modifiedDate='" + modifiedDate + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/pro355humanresources/RestController/EmployeeRestController.java
+++ src/main/java/com/example/pro355humanresources/RestController/EmployeeRestController.java
@@ -1,4 +1,23 @@
 package com.example.pro355humanresources.RestController;
 
+import com.example.pro355humanresources.BusinessLogicLayer.EmployeeLogicLayer;
+import com.example.pro355humanresources.Models.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeRestController {
+
+    private EmployeeLogicLayer employeeLogicLayer;
+
+    @GetMapping(path="/employees")
+    public List<Employee> findAllEmployees() throws IOException {
+        return employeeLogicLayer.findAllEmployees();
+    }
+
 }
