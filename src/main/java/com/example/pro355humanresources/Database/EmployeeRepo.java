0b54abfc0d25fc1228f6b3b24296148c47bccf83
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2022-11-30 23:49:09 -0700

    removed unused code

--- src/main/java/com/example/pro355humanresources/Database/EmployeeRepo.java
+++ src/main/java/com/example/pro355humanresources/Database/EmployeeRepo.java
@@ -11,7 +11,5 @@ public interface EmployeeRepo
     List<Employee> findAllByEmployeeFirstNameContaining(String employeeLastName);
     List<Employee> findAllByEmployeeLastNameContaining(String employeeLastName);
     List<Employee> findByEmployeeStatus(boolean enabled);
-   // List<Employee> findByEmployeeFirstName(String employeeFirstName);
-//    public Employee findByEmployeeId(String id);
-//    public Employee findByEmployeeFirstName(String employeeFirstName);
+
 }
