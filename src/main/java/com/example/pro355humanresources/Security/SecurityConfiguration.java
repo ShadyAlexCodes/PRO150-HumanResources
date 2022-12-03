79a455ed0b9a9c9956b95cb829f6c6f71403bc8b
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2022-12-02 18:43:29 -0700

    Updated security config and index and login links

--- src/main/java/com/example/pro355humanresources/Security/SecurityConfiguration.java
+++ src/main/java/com/example/pro355humanresources/Security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.pro355humanresources.Security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,7 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/api/**").permitAll()
-                .antMatchers("/index.html").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable() // Disable cross site request forgery checking
                 .sessionManagement().disable()
